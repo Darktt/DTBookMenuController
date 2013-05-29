23ed7cf24690377612563baf49b0e9cafa8e8b48
darktt <pippipig@gmail.com>
2013-05-29 20:50:20 +0800

    Initial Commit

---
+++ DTBookMenuControllerDemo/DTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DTAppDelegate.h
+//  DTBookMenuControllerDemo
+//
+//  Created by Darktt on 13/5/29.
+//  Copyright (c) 2013 Darktt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DTBookMenuControllerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DTBookMenuControllerDemo
+//
+//  Created by Darktt on 13/5/29.
+//  Copyright (c) 2013 Darktt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DTAppDelegate class]));
+    }
+}
